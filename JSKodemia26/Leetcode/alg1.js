0458f613d7e27a38b62658ed7f4214f91e0c37f5
MaganaTony <therealanthonymagana>
2023-07-25 20:02:03 -0600

    moved files to dirs

---
+++ JSKodemia26/Leetcode/alg1.js
@@ -0,0 +1,6 @@
+const arreglo = [1,2,3,4];
+const objeto = {dos : 'jake', uno: 'perro'}
+
+const proto = new Proto()
+
+console.log(Object.getPrototypeOf(objeto));
---
+++ JSKodemia26/clase-APIs/fetch.js
@@ -0,0 +1,5 @@
+const response = fetch('https://jsonplaceholder.typicode.com/',{
+    method: 'GET'
+});
+
+console.log(response);
