8368d7c51974a48ea7a4cd92eefc025defe2cb4c
MaganaTony <therealanthonymagana>
2023-07-25 21:41:43 -0600

    Added API class from today

--- JSKodemia26/Leetcode/alg1.js
+++ JSKodemia26/Leetcode/alg1.js
@@ -1,6 +1,8 @@
-const arreglo = [1,2,3,4];
-const objeto = {dos : 'jake', uno: 'perro'}
-
-const proto = new Proto()
-
-console.log(Object.getPrototypeOf(objeto));
+const myObject = {
+    city: "Madrid",
+    greet() {
+      console.log(`Greetings from ${myObject.city}`);
+    },
+  };
+  
+console.log(Object.getPrototypeOf(myObject));
--- JSKodemia26/clase-APIs/fetch.js
+++ JSKodemia26/clase-APIs/fetch.js
@@ -1,5 +1,34 @@
-const response = fetch('https://jsonplaceholder.typicode.com/',{
-    method: 'GET'
-});
+// const response = fetch('https://jsonplaceholder.typicode.com/posts/1/comments',{
+//     method: 'GET'
+// });
 
-console.log(response);
+// response
+// .then((data) => data.json())
+// .then((json) => console.log(json))
+
+//ASYNC //AWAIT
+
+const getInfo = async() => {
+
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts/1/comments',{
+            method: 'GET'
+        });
+        if (response.status === 200){
+            const data = await response.json()
+            createEmails(data)
+        } else {
+            alert('Hubo un error')
+        }
+    } catch (error) {
+        console.log(error, 'Hubo un error')
+    }
+};
+getInfo();
+
+const createEmails = (array) => {
+    const newList = array.map((item) => {
+        return item.email
+    });
+    console.log(newList);
+};
